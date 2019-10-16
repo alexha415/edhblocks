a94069b73a79b2c2ce75a315687997b55c201eb8
Alex Ha <aha2@mail.sfsu.edu>
2019-10-16 13:15:29 -0700

    need to separate each li into own component to keep track of its own state

--- src/components/deck/CardTypeList.js
+++ src/components/deck/CardTypeList.js
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {connect} from 'react-redux';
 import {removeFromDeck} from '../../actions/deckActions';
+import ImageModal from '../modal/ImageModal';
+
 const CardTypeList = ({list, category, removeFromDeck}) => {
+
+    const [show, setShow] = useState(false);
+
+    
+    const showModal = () => {
+        setShow(true);
+    }
+    const hideModal = () => {
+        setShow(false);
+    }
+
     return (
         <div style={{margin: '2rem'}}>
             <h4>{category}: {list ? list.length : '0'}</h4>
             <ul>
                 {list && list.map( card => {
                     return(
-                    <li key={card.name}>
-                        <a href="#/" onClick={() => {
-                            removeFromDeck(card)
-                        }}>{card.name}</a>
+                    <li key={card.name} className="deck-text">
+                        <div className='deck-item' onMouseOver={showModal} onMouseLeave={hideModal}>
+                            <ImageModal show={show} img={card.image}/>
+                            <a href="#/">
+                                <span>{card.name}</span>
+                            </a>
+                        </div>
                     </li>)
                 })}
-            </ul>
+            </ul>   
         </div>
     )
 }
