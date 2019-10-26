d4a9c3be6e04401dcc60d100488068c61833323e
Alex Ha <aha2@mail.sfsu.edu>
2019-10-25 23:11:16 -0700

    switched project structure for express server in root dir

---
+++ client/build/precache-manifest.5892c9262bbb03cc3ed7fdec35c169ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8c318a57a3ee59972091ed64ec465f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b956304947736f0f8b6",
+    "url": "/static/css/main.10ee650c.chunk.css"
+  },
+  {
+    "revision": "65e5a9288e7de684cc14",
+    "url": "/static/js/2.6642d58c.chunk.js"
+  },
+  {
+    "revision": "4b956304947736f0f8b6",
+    "url": "/static/js/main.5867136b.chunk.js"
+  },
+  {
+    "revision": "0434349b9bf542bc4f99",
+    "url": "/static/js/runtime-main.47caf56e.js"
+  }
+]);
--- server.js
+++ server.js
@@ -8,21 +8,20 @@ app.use(express.json({extended: false}));
 app.use(cors());
 
 
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.use('/api/users', require('./api/users'));
 app.use('/api/decks', require('./api/decks'));
 app.use('/api/auth', require('./api/auth'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname,'client/build/index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-
 app.listen(PORT, () => {
   console.log('Listening on : ' + PORT);
 })
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname,'/build/index.html'));
-});
-
