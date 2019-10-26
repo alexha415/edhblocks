c9388a82febe7d99cd65617951742d237034c94e
Alex Ha <aha2@mail.sfsu.edu>
2019-10-25 22:18:39 -0700

    attemping deploy on heroku

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 app.use(express.json({extended: false}));
 app.use(cors());
+
 app.use('/api/users', require('./api/users'));
 app.use('/api/decks', require('./api/decks'));
 app.use('/api/auth', require('./api/auth'));
@@ -15,6 +17,12 @@ connectDB();
 app.get('/', (req,res) => {
   res.send("Hello World");
 });
+
+if(process.env.NODE_ENV === 'production'){
+
+  app.use(express.static('build'))
+  app.get('*', (req, res) => sendFile(path.resolve(__dirname, '/build', 'index.html')));
+}
 
 app.listen(PORT, () => {
   console.log('Listening on : ' + PORT);
